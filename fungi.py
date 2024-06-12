ab0b8535daab8f3b31ab3cf4d68700a838a2f89e
Adam Garza <168875175+hyerland@users.noreply.github.com>
2024-06-12 00:43:02 -0500

    ✨feature: Change how logging work internally. Much props to `its_lino` on discord.

--- fungi.py
+++ fungi.py
@@ -1,25 +1,44 @@
-"""
-Discord bot for hosting self-hosted servers.
-"""
 import discord
+import json
+import logging
+from discord.ext import commands
 from rich import print
+from rich.console import Console
+from rich.logging import RichHandler
+
+# * Load configuration
+with open('fungi.json') as f:
+    config = json.load(f)
+
+#* Set up logging
+# FORMAT = "[orange]Fungi Runtime[/orange] - %(message)s"
+# formatter = logging.Formatter(FORMAT)
+
+log = logging.getLogger("fungi")
+log.addHandler(RichHandler(markup = True, console = Console()))
+
+    
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-client = discord.Client(intents=intents)
+client = commands.Bot(command_prefix="!", intents=intents)
 
 @client.event
 async def on_ready():
     print(f'[green][{client.user}][/green] Logged in successfully! :star:')
 
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
+@client.command()
+async def test(ctx):
+    await ctx.send('test')
+    log.debug(f"{ctx.author} ran the `test` command")
 
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
+def logLevel() -> int:
+    if config['settings']['runtime_logs']:
+        log.debug("Runtime logs enabled")
+        return logging.DEBUG
+    else:
+        return logging.INFO
 
 if __name__ == '__main__':
-    client.run('Hidden')
+    client.run(config['token'], log_handler=RichHandler(markup = True, console = Console()), log_level = logLevel())
--- manage.py
+++ manage.py
@@ -4,29 +4,73 @@ The layer between you, as the user, and the bot. Used for managing the bot.
 import typer
 import subprocess
 import logging
-from rich import print
+import os
+import json
+from rich import print, print_json
 from rich.logging import RichHandler
+from rich.prompt import Prompt, Confirm
+from typing_extensions import Annotated
 
-FORMAT = "[blue]Fungi CLI[/blue] - %(message)s"
-logging.basicConfig(
-    level="NOTSET", format=FORMAT, datefmt="[%X]", handlers=[RichHandler(markup=True)]
-)
+#* Set up logging
+#FORMAT = "[blue]Fungi CLI[/blue] - %(message)s"
+#logging.basicConfig(
+#    level="ERROR", format=FORMAT, datefmt="[%X]", handlers=[RichHandler(markup=True)]
+#)
 
-log = logging.getLogger("rich")
+#log = logging.getLogger("rich")
+
+
+#* Set up CLI application
 app = typer.Typer(add_completion=False)
 
+#? Metadata
+version = "0.1.0"
 
 @app.callback()
-def callback():
+def callback(verbose: Annotated[bool, typer.Option("--verbose", "-v")] = False):
     """
     Provides the bot with information needed to run for individual hosting. 
     """
-    log.info("Starting fungi framework...")
+    if verbose:
+        print('y')
+        #log.setLevel("DEBUG")
+        #log.debug("Verbose mode enabled")
 
+    print(f"Starting [red]Fungi [b]v{version}[/b][/red] framework...")
+    config_file = 'fungi.json'
+
+    #log.debug("Checking if config file exists...")
+    if not os.path.isfile(config_file):
+        #log.debug("Config file does not exist, prompting first time setup...")
+
+        print("Starting first time setup...")
+
+        token = Prompt.ask("\n[dim](https://discord.com/developers)[/dim]\nEnter your discord bot token", password=True)
+        prefix = Prompt.ask("What prefix would you like to use? (!ping or $ping)")
+
+        temp = {
+                "token": token,
+                "bot": {
+                    "prefix": prefix
+                }
+            }
+
+        print_json(data=temp, indent=4)
+
+        confirm = Confirm.ask("Generate configuration?", default=True)   
+        if confirm:
+            with open(config_file, 'w') as f:
+                json.dump(temp, f, indent=4)
+                f.write('\n')
+        else:
+            print("Exiting...")
+            exit(1)
+
+        print("[green]First time setup complete! :rocket:")
 
 @app.command()
 def start():
-    log.info("Starting bot services...")
+    #log.info("Starting bot services...")
     subprocess.run(["python", "-m", "fungi"], shell=True)
 
 
